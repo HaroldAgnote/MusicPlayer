67725e181aa3b3b3084e5bbea90719be29242f12
Jennifer <Jennifer@134.139.144.187>
2016-04-02 23:45:28 -0700

    i did a thing

--- MusicPlayer/src/Library.java
+++ MusicPlayer/src/Library.java
@@ -11,7 +11,7 @@ public class Library
 	private ArrayList <MediaPlayer> mpSongs;
 	private MediaPlayer mpCurrent;
 	private Song current; //Update Current Song
-	
+	private int i;
 	public Library()
 	{
 		songs = new ArrayList <Song> ();
@@ -42,44 +42,61 @@ public class Library
 		}
 	}
 	
-	public MediaPlayer getSong(int i)
+	public MediaPlayer getMP(int i)
 	{
 		//Update Current Song
 		mpCurrent = mpSongs.get(i);
 		return mpCurrent;
 	}
+	public Song getSong(int i)
+	{
+		current = songs.get(i);
+		return current;
+	}
 	
 	public void playFirst()
 	{
 		//Update Current Song
-		mpCurrent = getSong(0);
+		mpCurrent = getMP(0);
 		playSong(mpCurrent);
+		current = getSong(0);
+		
 	}
 	
 	public void playRandom()
 	{
 		//Update Current Song
 		int random = (int) (Math.random()*mpSongs.size());
-		mpCurrent = getSong(random);
+		mpCurrent = getMP(random);
 		playSong(mpCurrent);
+		current = getSong(random);
 	}
 	
 	public void playSong(MediaPlayer s)
 	{	
 		//Song is playing
 		s.play();
+	
+	}
+	public void playNext(MediaPlayer N)
+	{
+		
+		current = getSong(i +1);
+		mpCurrent = getMP(i+1);
 	}
 	
 	public void pausePlayback()
 	{
 		//Song is paused
 		mpCurrent.pause();
+		System.out.println(current + " is paused");
 	}
 	
 	public void stopPlayback()
 	{
 		//Song has stopped
 		mpCurrent.stop();
+		System.out.println(current + " is stopped");
 	}
 	
 	public void displaySongs()
